42f5d3cd3e9055d39d555110de19aedbd99875a6
Vyper-v <davidgomez2924@gmail.com>
2022-04-19 17:32:10 -0400

    refactor

--- desafio1.js
+++ desafio1.js
@@ -1,31 +1,41 @@
-class Usuario{
-	constructor(nombre,apellido,libros=[],mascotas=[]){
-		this.nombre=nombre;
-		this.apellido=apellido;
-		this.libros=libros;
-		this.mascotas=mascotas;
-	}
-	
-	getFullName(){
-		return this.nombre +' '+this.apellido
-	}	
-	addMascota(mascotaName){
-		this.mascotas.push(mascotaName)
-	}
-	countMascotas(){
-		return this.mascotas.length
-	}
-	addBook(bookName,autorName){
-		this.libros.push({nombre:bookName,autor:autorName})
-	}
-	getBookNames(){
-		return this.libros.map(({nombre})=>nombre)
-	}
+class Usuario {
+  constructor(nombre, apellido, libros = [], mascotas = []) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.libros = libros;
+    this.mascotas = mascotas;
+  }
+
+  getFullName() {
+    return this.nombre + " " + this.apellido;
+  }
+
+  addMascota(mascotaName) {
+    this.mascotas.push(mascotaName);
+  }
+
+  countMascotas() {
+    return this.mascotas.length;
+  }
+
+  addBook(bookName, autorName) {
+    this.libros.push({ nombre: bookName, autor: autorName });
+  }
+
+  getBookNames() {
+    return this.libros.map(({ nombre }) => nombre);
+  }
 }
 
-const David = new Usuario('David','Gomez');
-console.log(David.getFullName())
-David.addMascota('Luna')
-console.log(David.countMascotas())
-David.addBook('Introduction to Algorithms','Thomas H. Cormen')
-console.log(David.getBookNames())
+const David = new Usuario("David", "Gomez");
+
+David.addMascota("Luna");
+David.addMascota("Maya");
+David.addMascota("Coco");
+David.addBook("Introduction to Algorithms", "Thomas H. Cormen");
+David.addBook("Clean Code", "Robert Cecil Martin");
+David.addBook("Clean Architecture", "Robert Cecil Martin");
+
+console.log(David.getFullName());
+console.log("Mascotas: " + David.countMascotas());
+console.log("Libros: " + David.getBookNames());
