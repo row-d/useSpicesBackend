204fd4767902d528a02d05b084965048a831db5c
David Gomez Pinto <67220312+Vyper-v@users.noreply.github.com>
2022-06-27 01:08:25 -0400

    Delete test directory

--- test/api.spec.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest'
-
-import app from '../src/server'
-
-describe('GET /', () => {
-  it('should return 200 OK', () => {
-    return request(app).get('/').expect(200)
-  })
-})
